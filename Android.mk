848ec2abaa20282ec0162207741c935c77760c56
TheScarastic <warabhishek@gmail.com>
2019-01-30 20:51:44 +0000

    power: Add support for event-based tap to wake gesture control
    Change-Id: I0bd3f28cd8756239b40af78081b75457fdaf8f12

--- Android.mk
+++ Android.mk
@@ -10,6 +10,7 @@ LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libutils android.hardware.power@1.2
 LOCAL_HEADER_LIBRARIES += libutils_headers
 LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_HEADER_LIBRARIES += generated_kernel_headers
 LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c service.cpp Power.cpp
 LOCAL_C_INCLUDES := external/libxml2/include \
                     external/icu/icu4c/source/common
@@ -86,6 +87,10 @@ endif
 
 ifeq ($(call is-board-platform-in-list,msmnile), true)
 LOCAL_SRC_FILES += power-msmnile.c
+endif
+
+ifneq ($(TAP_TO_WAKE_EVENT_NODE),)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TAP_TO_WAKE_EVENT_NODE)\"
 endif
 
 ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
