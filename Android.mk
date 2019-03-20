e142db4ff3334da743879230f3208c22df3d378c
TheStrix <parthbhatia98@gmail.com>
2019-03-20 13:23:08 +0530

    power: Allow shipping legacy HAL to system and then bind mount it
    Change-Id: I40234144b7d4b81edb3caaf5686c7e62e469cc4e

--- Android.mk
+++ Android.mk
@@ -92,8 +92,12 @@ ifneq ($(TARGET_USES_NON_LEGACY_POWERHAL), true)
 LOCAL_MODULE := power.qcom
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
-LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
+  ifeq($(TARGET_POWER_BINDMOUNT_VENDOR),)
+    LOCAL_VENDOR_MODULE := true
+  else
+    LOCAL_INIT_RC := power.qcom.rc
+  endif
 else
 
 LOCAL_MODULE := android.hardware.power@1.2-service
