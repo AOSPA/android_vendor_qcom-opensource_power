4a1c5e7c2c2f65cd2b14639d13fff5f7a95079de
Arian <arian.kulmer@web.de>
2020-03-09 18:04:55 +0100

    power: Move legacy check to Android.mk
    Change-Id: Ibf85658e5d0734d733b2dfe16fd2c555888267c7

--- Android.mk
+++ Android.mk
@@ -2,6 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 
+# Disable Power HAL by default for legacy targets.
+# Devices can still opt in by setting TARGET_USES_NON_LEGACY_POWERHAL in BoardConfig.mk.
+# Conversely, recent chips, such as sm8150 with an old vendor can opt out.
+ifeq ($(call is-board-platform-in-list, msm8937 msm8952 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845 sdm710 trinket),true)
+TARGET_USES_NON_LEGACY_POWERHAL ?= true
+endif
+
 # HAL module implemenation stored in
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
